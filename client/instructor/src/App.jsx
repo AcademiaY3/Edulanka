9c178846c097baf8cca49bb2365a3841721da4be
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2024-05-14 16:27:25 +0530

    addedRoutesInstructor

--- client/instructor/src/App.jsx
+++ client/instructor/src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Main from './Pages/Main'
+import { Route, Routes } from 'react-router-dom'
+import Login from './Pages/Login/Login'
+import DashBoard from './Pages/UserStack/Dashboard/DashBoard'
+import CourseM from './Pages/UserStack/CourseM/CourseM'
+import UserStack from './Pages/UserStack/UserStack'
+import LearnerM from './Pages/UserStack/LearnerM/LearnerM'
+import Orders from './Pages/UserStack/Orders/Orders'
+import Payments from './Pages/UserStack/Payments/Payment'
+import Remainder from './Pages/UserStack/Remainder/Remainder'
+import MyProfile from './Pages/UserStack/MyProfile/MyProfile'
+import AddCourse from './Pages/UserStack/CourseM/AddCourse'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Main/>
+      <Routes>
+        <Route path='/' element={<Login/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/main' element={<UserStack />} >
+          <Route index element={<DashBoard/>} />
+          <Route path='dashboard' element={<DashBoard/>} />
+          <Route path='courses' element={<CourseM/>} />
+          <Route path='addcourse' element={<AddCourse/>} />
+          <Route path='learners' element={<LearnerM/>} />
+          <Route path='orders' element={<Orders/>} />
+          <Route path='payments' element={<Payments/>} />
+          <Route path='remainder' element={<Remainder/>} />
+          <Route path='profile' element={<MyProfile/>} />
+        </Route>
+      </Routes>
     </>
   )
 }
